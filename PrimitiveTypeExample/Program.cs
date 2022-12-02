cede6b91c1e4df73ef6761771e720f9343339fec
oluwaseunoa <oluwaseun.beicks@gmail.com>
2022-12-02 16:05:06 +0100

    Add project files.

---
+++ PrimitiveTypeExample/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace PrimitiveTypeExample
+{
+    class PrimitiveDataExample
+    {
+        static void Main()
+        {
+            sbyte a = sbyte.MaxValue;
+            sbyte b = 100;
+            sbyte c = default(sbyte);
+
+            byte d = byte.MaxValue;
+            short e = short.MaxValue;
+            ushort f = ushort.MaxValue;
+            int g = int.MaxValue;
+            uint h = uint.MaxValue;
+            long i = long.MaxValue;
+            ulong j = ulong.MaxValue;
+            float k = float.MaxValue;
+            double l = double.MaxValue;
+            decimal m = decimal.MaxValue;
+            char n = 'A';
+            string o = "abc";
+            bool p = true;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+            Console.WriteLine(k);
+            Console.WriteLine(l);
+            Console.WriteLine(m);
+            Console.WriteLine(n);
+            Console.WriteLine(o);
+            Console.WriteLine(p);
+
+            System.Console.ReadKey();
+
+        }
+    }
+}
